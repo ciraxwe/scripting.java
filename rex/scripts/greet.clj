5123b8373ed13bb7c3d198c1a9e9cef2e2b9c616
Emanuelle Garduno <emanuelle.garduno@gmail.com>
2013-03-23 11:03:34 -0400

    Removing rexr/scripts. Content will be moved to master

--- rex/scripts/greet.clj
+++
@@ -1,7 +0,0 @@
-(ns .
-  (:require [clojure.string :as s]))
-
-(defn greet [who]
-    (println(str "Hello " (s/capitalize who) "!")))
-
-(greet "world") ; output "Hello World!
--- rex/scripts/greet.py
+++
@@ -1,9 +0,0 @@
-class Greet:
-    def __init__(self, who):
-        self.name = who.capitalize()
-
-    def salute(self):
-        print "Hello %s!" % self.name
-
-g = Greet("world") # create object
-g.salute() # output "Hello World!"
