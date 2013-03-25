52479f2d4859a70220dfed34ddb826164ba44fed
Emanuelle Garduno <emanuelle.garduno@gmail.com>
2013-03-25 12:34:27 -0400

    Added rex.zip and changes to Install.ps1

--- rex/Install.ps1
+++ rex/Install.ps1
@@ -1,22 +1,45 @@
-# variables
-$url = "http://localhost:8080/rex/rex.zip"
+# Functions
+# -----------------------------------------------------------------------------
+function Get-WebFile($url, $directory) {
+    $fileName = [System.IO.Path]::GetFileName($url)
+    $file = Join-Path $directory $fileName
+
+    Write-Host "Downloading $url to $file"
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($url, $file)
+
+    return $file
+}
+
+function Unzip($file, $folder) {
+    Write-Host "Extracting $file to $folder"
+    $shell = New-Object -com shell.application
+    $zipPackage = $shell.NameSpace($file)
+    $extact = $shell.NameSpace($folder)
+    $extact.CopyHere($zipPackage.Items(), 0x10)
+}
+
+
+# Variables
+# -----------------------------------------------------------------------------
 $rexHome = Join-Path $pwd "rex"
 
 if (-not (Test-Path $rexHome)) {
     New-Item -ItemType Directory -Path $rexHome | Out-Null
 }
 
-$fileName = [System.IO.Path]::GetFileName($url)
-$file = Join-Path $rexHome $fileName
+# Install REX
+# -----------------------------------------------------------------------------
 
-# download the package
-Write-Host "Downloading $url to $file"
-$downloader = New-Object System.Net.WebClient
-$downloader.DownloadFile($url, $file)
+$file = Get-WebFile "http://ciraxwe.github.com/scripting.java/rex/rex.zip" $rexHome
+#$file = Get-WebFile "http://localhost:8080/rex/rex.zip" $rexHome
+Unzip $file $rexHome
+Remove-Item $file
 
-# unzip the package
-Write-Host "Extracting $file to $rexHome..."
-$shellApplication = New-Object -com shell.application
-$zipPackage = $shellApplication.NameSpace($file)
-$destinationFolder = $shellApplication.NameSpace($rexHome)
-$destinationFolder.CopyHere($zipPackage.Items(),0x10)
+# Install Examples
+# -----------------------------------------------------------------------------
+
+$file = Get-WebFile "https://github.com/ciraxwe/scripting.java/archive/master.zip" $rexHome
+Unzip $file $rexHome
+Remove-Item $file
+Move-Item (Join-Path $rexHome scripting.java-master) (Join-Path $rexHome scripts)
